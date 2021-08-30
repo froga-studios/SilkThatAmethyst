6c0fd0ccd16bab77d1f26245d501a9e3f1787255
ExoPlant <lennyj2468+github@gmail.com>
2021-08-30 11:52:05 +0100

    Mod works

---
+++ src/main/java/com/frogastudios/silkthatamethyst/SilkThatAmethyst.java
@@ -0,0 +1,10 @@
+package com.frogastudios.silkthatamethyst;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SilkThatAmethyst implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/frogastudios/silkthatamethyst/client/SilkThatAmethystClient.java
@@ -0,0 +1,13 @@
+package com.frogastudios.silkthatamethyst.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class SilkThatAmethystClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
